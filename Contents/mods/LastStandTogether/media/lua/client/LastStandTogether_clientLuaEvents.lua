291f0b473e2629e00d2d778e6f67fb8f2e11f38a
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2025-08-13 19:40:19 -0400

    Update LastStandTogether_clientLuaEvents.lua

--- Contents/mods/LastStandTogether/media/lua/client/LastStandTogether_clientLuaEvents.lua
+++ Contents/mods/LastStandTogether/media/lua/client/LastStandTogether_clientLuaEvents.lua
@@ -4,3 +4,7 @@ Events.OnPostFloorLayerDraw.Add(zoneRender.drawZoneEffects)
 local onZombie = require "LastStandTogether_onZombie.lua"
 Events.OnZombieUpdate.Add(onZombie.update)
 Events.OnObjectCollide.Add(onZombie.collide)
+
+require "shop-clientEvents.lua"
+local shopMarkerSystem = require "shop-markers.lua"
+Events.OnPostFloorLayerDraw.Remove(shopMarkerSystem.render)
